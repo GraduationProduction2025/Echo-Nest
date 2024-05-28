e560753625e4a7954c18f28ad6bf96697e0c3fd7
se-ma2024 <94701414+se-ma2024@users.noreply.github.com>
2024-05-28 14:33:31 +0900

    users,adminsデータベースの追加

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Users, Admins
+
+class LoginAdmin(admin.ModelAdmin):
+    list_display = ('Users', 'Admins')
+
+admin.site.register(Users)
+admin.site.register(Admins)
--- login/models.py
+++ login/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# ユーザのデータベースの作成
+class Users(models.Model):
+    user_id = models.IntegerField(primary_key = True)
+    user_name = models.CharField(max_length = 100)
+    user_pass = models.CharField(max_length = 100)
+    user_pass_v = models.CharField(max_length = 100)
+
+# 管理者のデータベースの作成
+class Admins(models.Model):
+    admin_id = models.IntegerField(primary_key = True)
+    admin_name = models.CharField(max_length = 100)
+    admin_pass = models.CharField(max_length = 100)
+    admin_pass_v = models.CharField(max_length = 100)
