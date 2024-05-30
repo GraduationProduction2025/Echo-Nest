0d5e96e592cd6a834b6e0a511e012c7ff0f510e4
Su_Pu#1A4472 <shown22327989@gmail.com>
2024-05-30 14:29:44 +0900

    mysql日本語化

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from surveys.models import Survey, Question, Choice, Choicetype
 
 # Register your models here.
+admin.site.register(Survey)
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Choicetype)
--- surveys/models.py
+++ surveys/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+# アンケートのデータベースの作成
+class Survey(models.Model):
+    id = models.IntegerField(primary_key = True)
+    title = models.CharField(max_length = 255)
+    url = models.URLField()
+    create_at = models.DateTimeField()
+    create_user = models.CharField(max_length = 255)   #users-user_idの外部キーを設定する
+    delete_flag = models.BooleanField(default = False, help_text = '削除済みならTrue')
+
+# 選択肢形式データベース
+class Choicetype(models.Model):
+    id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length = 255)
+
+# 質問データベース
+class Question(models.Model):
+    id = models.IntegerField(primary_key = True)
+    title = models.CharField(max_length = 255)
+    survey_id = models.ForeignKey(Survey, on_delete = models.CASCADE)
+    type_id = models.ForeignKey(Choicetype, on_delete = models.CASCADE)
+
+# 選択肢データベース
+class Choice(models.Model):
+    id = models.IntegerField(primary_key = True)
+    text = models.CharField(max_length = 255)
+    question_id = models.ForeignKey(Question, on_delete = models.CASCADE)
