9d9dcf4f6cf33bf807e44a0294df4733a2a55f4f
Su_Pu#1A4472 <shown22327989@gmail.com>
2024-06-10 16:44:20 +0900

    データベースの取得と表示

--- surveys/urls.py
+++ surveys/urls.py
@@ -6,4 +6,5 @@ app_name = 'templates'
 
 urlpatterns = [
     path('list/', views.index, name='list'),
+    path('info/', views.info, name='info'),
 ]
--- surveys/views.py
+++ surveys/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import Survey
 
 def index(request):
     base = {
         'title':'質問一覧'
     }
     return render(request,'surveys/list_ques.html',base)
+
+# データベースを取得して表示する
+def info(request):
+    survey = Survey.objects.all()
+    survey_2 = Survey.objects.values()
+    infodict = {
+        'title':'テスト',
+        'val':survey,
+        'val2':survey_2,
+    }
+    return render(request, 'surveys/list.html', infodict)
