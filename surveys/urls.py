21d1589d7139df8d99308c4abebab04d14c2f561
いぶ吉 <ito.38101112@gmail.com>
2024-06-25 13:43:19 +0900

    views-indexとviews.infoをviews-listに統合

--- surveys/urls.py
+++ surveys/urls.py
@@ -5,8 +5,7 @@ from . import views
 app_name = 'templates'
 
 urlpatterns = [
-    path('list/', views.index, name='list'),
-    path('info/', views.info, name='info'),
+    path('list/', views.list, name='list'),
     path('detail/<int:survey_id>/', views.detail, name='detail'),
     path('add/', views.add, name='add'),
     path('create/', views.create, name='create'),
--- surveys/views.py
+++ surveys/views.py
@@ -3,25 +3,19 @@ from .models import Survey, Question, Choice, Choicetype
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-def index(request):
-    base = {
-        'title':'質問一覧'
-    }
-    return render(request,'surveys/list_ques.html',base)
-
 # データベースを取得して表示する
-def info(request):
+def list(request):
     survey = Survey.objects.all()
     survey_2 = Survey.objects.values()
     # header = ['ステータス','質問タイトル','URL','作成日','作成ユーザ']
     header = ['ステータス','質問タイトル','作成日','作成ユーザ','詳細']
-    infodict = {
+    listdict = {
         'title':'テスト',
         'header':header,
         'val':survey,
         'val2':survey_2,
     }
-    return render(request, 'surveys/info.html', infodict)
+    return render(request, 'surveys/list_ques.html', listdict)
 
 # データベースの内容を取得して表示
 def detail(request, survey_id):
