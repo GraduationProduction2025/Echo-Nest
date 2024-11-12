3ab17fad370595dcede4f03e7ee056a2b9d7463a
いぶ吉 <ito.38101112@gmail.com>
2024-11-12 14:18:02 +0900

    merge時の微調整

--- surveys/views.py
+++ surveys/views.py
@@ -1,51 +1,22 @@
 from django.shortcuts import render, redirect
-from .models import Survey, Question, Choice, Choicetype
+from .models import Survey, Question, Choice, Choicetype, Answer
+from django.http import HttpResponse, Http404
+from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 from django.db import models
 import re
 
 # データベースを取得して表示する
-def list(request):
-    survey = Survey.objects.all()
-    survey_2 = Survey.objects.values()
-    # header = ['ステータス','質問タイトル','URL','作成日','作成ユーザ']
-    header = ['ステータス','質問タイトル','作成日','作成ユーザ','詳細']
+def list_ques(request):
+    survey_field_data = Survey.objects.values()
     listdict = {
-        'title':'テスト',
-        'header':header,
-        'val':survey,
-        'val2':survey_2,
+        'title':'一覧',
+        'val':survey_field_data,
     }
     return render(request, 'surveys/list_ques.html', listdict)
 
-# データベースの内容を取得して表示
-def detail(request, survey_id):
-    survey = Survey.objects.get(id=survey_id)
-    questions = Question.objects.filter(survey=survey)
-    choices = Choice.objects.filter(question__in=questions)
-    context = {
-        'survey': survey,
-        'questions': questions,
-        'choices': choices,
-    }
-    return render(request, 'surveys/detail.html', context)
-
-# テスト
-def add(request):
-    base = {
-        'title':'アンケート追加'
-    }
-    return render(request,'surveys/add.html',base)
-    # return HttpResponse('add')
-
 def create_ques(request):
-    # GETリクエストの場合、入力フォームを表示
-    choicetypes = Choicetype.objects.all()  # 質問タイプを選ぶために全てのタイプを取得
-    # base辞書を作成
-    base = {
-        'title': 'アンケート作成',
-        'choicetypes': choicetypes,  # 質問タイプを追加
-    }
     survey = Survey.objects.all()
     survey_2 = Survey.objects.values()
     # header = ['ステータス','質問タイトル','URL','作成日','作成ユーザ']
@@ -134,5 +105,83 @@ def create_ques(request):
 
         return render(request, 'surveys/list_ques.html', listdict)
 
-    return render(request, 'surveys/create_ques.html', base)
+    return render(request, 'surveys/create_ques.html', listdict)
 
+
+def al_list(request):
+    survey_field_data = Survey.objects.filter(published_flag=True)
+    listdict = {
+        'title':'公開済みアンケート一覧',
+        'val':survey_field_data,
+    }
+    return render(request, 'surveys/al_list.html', listdict)
+
+def tem_list(request):
+    survey_field_data = Survey.objects.filter(published_flag=False)
+    listdict = {
+        'title':'下書きアンケート一覧',
+        'val':survey_field_data,
+    }
+    return render(request, 'surveys/tem_list.html', listdict)
+
+def ag_data(request, survey_id):
+    survey_field_data = Survey.objects.get(id = survey_id)
+    listdist = {
+        'title':'集計結果',
+        'val':survey_field_data
+    }
+    return render(request, 'surveys/ag_data.html', listdist)
+
+def edit_ques(request, survey_id):
+    survey = Survey.objects.get(id = survey_id)
+    listdict = {
+        'title':'編集画面',
+        'survey':survey,
+    }
+    return render(request, 'surveys/edit_ques.html', listdict)
+
+def answer(request, survey_id):
+    try:
+        # ページ番号と同じSurveyを取り出す
+        survey = Survey.objects.get(id=survey_id)
+        # 上のアンケートに関連したQuestionを取り出す
+        question = Question.objects.filter(survey__id = survey_id)
+        listdict = {
+            "survey": survey,
+            "question": question,
+            "title": survey.title,
+        }
+    except Survey.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "answers/answer.html", listdict)
+
+@csrf_exempt  # CSRF保護を一時的に無効にする（開発中のみ）
+def complete(request):
+    if request.method == 'POST':
+        responses = request.POST
+        response_list = []
+
+        for key, value in responses.items():
+            if key != 'csrfmiddlewaretoken':
+                question_id = int(key)
+                question = Question.objects.get(id=question_id)  # 質問を取得
+                answer_data = {
+                    "question_id": question.id,  # 質問のIDを取得
+                    "type": str(question.type),  # 質問のタイプを取得
+                    "content": [value],  # ユーザーが入力した回答をリスト形式で格納
+                }
+
+                # Answerインスタンスを作成
+                answer_instance = Answer(
+                    context=answer_data,  # JSONデータをcontextに格納
+                    question=question  # Questionを関連付け
+                )
+                answer_instance.save()  # データベースに保存
+                response_list.append(answer_data)
+        listdict = {
+            'title': '回答完了画面',
+            'responses': response_list,
+        }        
+        return render(request, 'answers/complete.html', listdict)
+
+    return render(request, 'answers/answer.html', {'title': '回答ページ'})
