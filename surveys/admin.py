0ace551398954b106e4eaec8663544d9b224f087
田中匠 <shown22327989@gmail.com>
2024-12-17 12:40:21 +0900

    ユーザの回答記録データベースの作成

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 # アンケートDBの読み取り
-from .models import Survey,Question,Choice,Choicetype,Answer
+from .models import Survey,Question,Choice,Choicetype,Answer,UsersAnswer
 
 # Register your models here.
 # adminサイトでアンケートのデータベースを見る
 class SurveysAdmin(admin.ModelAdmin):
-    list_display = ('Survey', 'Question', 'Choice', 'Choicetype', 'Answer')
+    list_display = ('Survey', 'Question', 'Choice', 'Choicetype', 'Answer', 'UsersAnswer')
 
 admin.site.register(Survey)
 admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(Choicetype)
 admin.site.register(Answer)
+admin.site.register(UsersAnswer)
